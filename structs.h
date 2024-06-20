af792852be561da59f5e317e6c53220396297af8
maurauca <maura.quintanilla15903@est.uca.edu.ni>
2024-06-20 09:35:05 -0600

    añadir struct y maximo del procedimiento producto

---
+++ structs.h
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+void menu();
+void cita();
+int servicios();
+void productos();
+
+struct Paciente{
+    string nombres; //std sol,uciona error del getline en agendar citas por alguna razon??? NO QUITAR()
+    string apellidos;
+    string numero;
+    string correo;
+    int mesCita;
+    int fecha;
+    int hora;
+    int segundos;
+    int servicio;
+    
+} paciente[100]; 
+
+//structs de productos kkke
+const int MAX_PROD = 5;
+const int MAX_CATEGORIAS = 3;
+
+
+
+struct Producto
+{
+    string nombre;
+    double precio_prod;
+};
+
+struct Categoria
+{
+    string nombre;
+    Producto productos[MAX_PROD];
+    int num_productos;
+};
+
+
+
+
+
+int cont = 0; // Contador para el número de pacientes
+
+void calendario();
+int calcDias();
+bool esBisiesto();
+int zeller();
+int getTime ();
+
+void fechas();
+void calculoFechas();
+
+int dia_mes,mes,year,mesop;
+
+#define NUM_ALLOWED 8
